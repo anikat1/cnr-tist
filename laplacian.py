94b69eef106923f977017f1fe895a47c736210d4
Anika Tabassum <anikat@Anikas-MBP.fios-router.home>
2020-05-18 14:28:36 -0400

    added datasets

--- laplacian.py
+++ laplacian.py
@@ -8,16 +8,22 @@ import networkx as nx
 import pandas as pd
 import sys
 from scipy.io import savemat
+from scipy.io import loadmat
+import numpy as np
 
 if __name__ == '__main__':
-	fileName = sys.argv[1]#'data/Harvey_60min_sample.csv';
-	adjlist = sys.argv[2]#'data/Harvey.adjlist';
-	savefile= sys.argv[3] #'data/Harvey_laplace.mat'
-	data = pd.read_csv(fileName)
-	counties = list(data.columns.values)
-	#print(len(counties))
-	G = nx.read_adjlist(adjlist,delimiter='\t')
-	L = nx.laplacian_matrix(G,counties)
-	print(L)
-	savemat(savefile,{'L':L})
+    fileName = sys.argv[1] #'../us_state_covid.csv'
+    adjlist = sys.argv[2] #'../us_states.adjlist'
+    savefile= sys.argv[3] #'../us_states'
+    data = pd.read_csv(fileName)
+    counties = list(data.columns.values)
+    #print(len(counties))
+    G = nx.read_adjlist(adjlist,delimiter=',')
+    print(G.edges)
+    L = nx.laplacian_matrix(G,counties)
+    np.savetxt(savefile+'.csv',L.toarray(),delimiter=',')
+    #print(L)
+    #savemat(savefile,{'L':L})
+    #mat_file = loadmat(savefile)
+    #print(mat_file['L'])
 
